0c00c2be462fbfe54a37fea5d78e917177b04572
Erinn Van der Sande <erinn.francis.van.der.sande@vub.be>
2022-07-28 10:09:43 +0200

    made it respond faster

--- mqtt_scripts/intent_handler/src/intent_handler.py
+++ mqtt_scripts/intent_handler/src/intent_handler.py
@@ -9,6 +9,7 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("hermes/tts/sayFinished")
     client.subscribe("hermes/button/#")
     client.subscribe('hermes/dialogueManager/sessionEnded')
+    client.subscribe('hermes/dialogueManager/sessionStarted')
     print("Connected. Waiting for intents to handle.",flush = True)
 
 
@@ -18,8 +19,15 @@ def on_disconnect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     """Called each time a message is received on a subscribed topic."""
+    global session_id
     if msg != "dummy":
         current_conversation.remove(msg.topic)
+    if not(isinstance(msg,str)):
+        if "hermes/dialogueManager/sessionStarted" == msg.topic:
+            payload = json.loads(msg.payload)
+            session_id = payload["sessionId"]
+        if "intent" in msg.topic:
+            client.publish("hermes/dialogueManager/endSession",json.dumps({"sessionId":session_id}))
     if current_conversation.can_proceed(): 
         old,idx = current_conversation.proceed()
         for publish in old.on_leave()[idx]:
@@ -28,7 +36,7 @@ def on_message(client, userdata, msg):
             client.publish(publish.get("topic"), json.dumps({k: v for k, v in publish.items() if k != "topic"}))
         on_message(client,userdata,"dummy")
 current_conversation = conversation.create_conversation_graph()
-
+session_id = ""
 # Create MQTT client and connect to broker
 client = mqtt.Client()
 client.on_connect = on_connect
